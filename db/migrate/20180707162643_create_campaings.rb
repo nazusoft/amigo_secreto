aab3cae74c60910bdba55b0792d8736e9e875fa8
cauecjm <cauecjm@gmail.com>
2018-07-09 19:07:32 -0300

    db/migrate: add 'create_campaign' e 'create_members'

---
+++ db/migrate/20180707162643_create_campaings.rb
@@ -0,0 +1,18 @@
+class CreateCampaings < ActiveRecord::Migration[5.2]
+
+  def change
+    create_table :campaings do |t|
+      t.string   :title
+      t.text     :description
+      t.datetime :event_date
+      t.string   :event_hour
+      t.string   :location
+      t.integer  :status
+
+      t.references :user, foreign_key: true
+
+      t.timestamps
+    end
+  end
+
+end
---
+++ db/migrate/20180707162947_create_members.rb
@@ -0,0 +1,16 @@
+class CreateMembers < ActiveRecord::Migration[5.2]
+
+  def change
+    create_table :members do |t|
+      t.string  :name
+      t.string  :email
+      t.boolean :open
+      t.string  :token
+
+      t.references :campaing, foreign_key: true
+
+      t.timestamps
+    end
+  end
+
+end
