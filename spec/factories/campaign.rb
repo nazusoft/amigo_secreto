74a25b9fb998d2cad3d68662fd949ab4bd410340
cauecjm <cauecjm@gmail.com>
2018-07-09 21:00:16 -0300

    spec/factories: add user.rb, campaign.rb e user.rb

---
+++ spec/factories/campaign.rb
@@ -0,0 +1,9 @@
+FactoryBot.define do
+
+ factory :campaign do
+   title       { FFaker::Lorem.word }
+   description { FFaker::Lorem.sentence }
+   user
+ end
+
+end
---
+++ spec/factories/member.rb
@@ -0,0 +1,9 @@
+FactoryBot.define do
+
+ factory :member do
+   name     { FFaker::Lorem.word }
+   email    { FFaker::Internet.email }
+   campaign
+ end
+
+end
