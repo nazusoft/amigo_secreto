1756dc7df56e6c50e73bf3656eb55092ccf22019
cauecjm <cauecjm@gmail.com>
2018-08-09 17:38:13 -0300

    spec/services/raffle_service_spec.rb: configurando testes

---
+++ spec/services/raffle_service_spec.rb
@@ -0,0 +1,72 @@
+require 'rails_helper'
+
+describe RaffleService do
+
+  before :each do
+    @campaign = create(:campaign, status: :pending)
+  end
+
+  describe '#perform' do
+    context "when has more then two members" do
+
+      before(:each) do
+        create(:member, campaign: @campaign)
+        create(:member, campaign: @campaign)
+        create(:member, campaign: @campaign)
+
+        @campaign.reload
+
+        @results = RaffleService.new(@campaign).perform
+      end
+
+      it "results is a hash" do
+        expect(@results.class).to eq(Hash)
+      end
+
+      it "all members are in results as a member" do
+        result_members = @results.mapi{|r| r.first }
+
+        expect(result_members.sort).to eq(@campaign.members.sort)
+      end
+
+      it "all member are in results as a friend" do
+        result_friends = @results.map {|r| r.last }
+
+        expect(result_friends.sort).to eq(@campaign.members.sort)
+      end
+
+      it "a member don't get yourself" do
+        @results.each do |r|
+          expect(r.first).not_to eq(r.last)
+        end
+      end
+
+      it "a member x don't get a member y that get the member x" do
+        @result.each do |r|
+          member = @result.select{|x| x.first == r.last }
+
+          expect(r.last).not_to eq(member.first)
+        end
+      end
+
+    end
+
+    context "when don't has more then two members" do
+
+      before(:each) do
+        create(:member, campaign: @campaign)
+
+        @campaign.reload
+
+        @response = RaffleService.new(@campaign).perform
+      end
+
+      it "return false" do
+        expect(@response).to eql(false)
+      end
+
+    end
+
+  end
+
+end
