5ceec222bb208a1d3e9a52dcb5e1fa161a9fa728
cauecjm <cauecjm@gmail.com>
2018-10-11 18:27:15 -0300

    campaigns_helper: adicionando método 'visibility'

--- app/helpers/campaigns_helper.rb
+++ app/helpers/campaigns_helper.rb
@@ -1,2 +1,17 @@
 module CampaignsHelper
+
+  def visibility(campaign)
+    qtd_opened = campaign.count_opened
+    qtd_total  = campaign.members.count
+    klass      = qtd_opened == qtd_total ? 'green-text' : ''
+
+    icon = "<i class='material-icons visibility-icon' style='margin-right:10px'>visibility</i>"
+
+    "<h6 class='visibility-text m-0 #{klass} valign-wrapper'>#{icon} Vistos #{qtd_opened}/#{qtd_total}</h6>".html_safe
+  end
+
+  def format_event_date(event_date)
+    event_date ? event_date.strftime("%d.%m.%y") : '--------'
+  end
+
 end
