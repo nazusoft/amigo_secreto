c35276c5c44c5387099235c791091d2e647af758
cauecjm <cauecjm@gmail.com>
2018-10-20 08:30:00 -0300

    config/routes: comentando rota para sidekiq

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
 
   devise_for :users, controllers: { registrations: 'registrations' }
 
-  mount Sidekiq::Web => '/sidekiq'
+  #mount Sidekiq::Web => '/sidekiq'
 
   resources :campaigns, except: [ :new ] do
     post 'raffle', on: :member
