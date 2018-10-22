92f6beb8512ef035b052302d908dbf009827c53e
cauecjm <cauecjm@gmail.com>
2018-10-22 20:47:48 -0300

    environments/production: setando action_mailer.default_url_option para utilizar variavel de ambiente 'MACHINE_IP'

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -57,7 +57,7 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "amigo_secreto_#{Rails.env}"
   config.action_mailer.perform_caching = false
 
-  config.action_mailer.default_url_options = { host: 'http://localhost:3000' }
+  config.action_mailer.default_url_options = { host: ENV['MACHINE_IP'] }
   config.action_mailer.delivery_method = :smtp
 
   config.action_mailer.raise_delivery_errors = true
