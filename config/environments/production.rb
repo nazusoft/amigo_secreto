730e76cc9b98f8322ffb27f02603aa60e40782bf
cauecjm <cauecjm@gmail.com>
2018-10-22 12:23:00 -0300

    environments/production: setando corretamente config.action_mailer.default_url_option

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -57,7 +57,7 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "amigo_secreto_#{Rails.env}"
   config.action_mailer.perform_caching = false
 
-#  config.action_mailer.default_url_options = { :host => request.host_with_port }
+  config.action_mailer.default_url_options = { host: 'http://localhost:3000' }
   config.action_mailer.delivery_method = :smtp
 
   config.action_mailer.raise_delivery_errors = true
@@ -69,7 +69,7 @@ Rails.application.configure do
     password:             ENV["SMTP_PASSWORD"],
     authentication:       :plain,
     enable_starttls_auto: true
-}
+  }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
