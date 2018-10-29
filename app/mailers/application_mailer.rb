5a449f1a9b4b2cbe5d4fed636c9d72cd8e02166b
cauecjm <cauecjm@gmail.com>
2018-10-29 20:38:12 -0300

    app/mailers/application_mailer: < Devise::Mailer, helper :application e include Devise::Controllers::UrlHelpers

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,10 @@
-class ApplicationMailer < ActionMailer::Base
+class ApplicationMailer < Devise::Mailer
+
+  helper :application
+
+  include Devise::Controllers::UrlHelpers
+
   default from: 'from@example.com'
   layout 'mailer'
+
 end
