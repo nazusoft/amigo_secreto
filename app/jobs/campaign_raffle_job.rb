2eb87234811419cbeeb152a5bb18b54f8d0b05b2
cauecjm <cauecjm@gmail.com>
2019-04-25 22:37:45 -0300

    campaign_raffle_job: add método 'send_error'

--- app/jobs/campaign_raffle_job.rb
+++ app/jobs/campaign_raffle_job.rb
@@ -1,20 +1,31 @@
 class CampaignRaffleJob < ApplicationJob
+
   queue_as :emails
 
   def perform(campaign)
-    results = RaffleService.new(campaign).perform
+    begin
+      results = RaffleService.new(campaign).perform
 
-    campaign.members.each{|m| m.set_pixel }
+      if results.empty? and results.blank?
+        send_error(campaign)
+      else
+        campaign.members.each{|m| m.set_pixel }
 
-    results.each do |r|
-      CampaignMailer.raffle(campaign, r.first, r.last).deliver_now
+        results.each do |r|
+          CampaignMailer.raffle(campaign, r.first, r.last).deliver_now
+        end
+
+        campaign.update(status: :finished)
+      end
+    rescue
+      send_error(campaign)
     end
+  end
 
-    campaign.update(status: :finished)
+  private
 
-    #if results == false
-      # Send mail to owner of campaign (desafio)
-    #end
+  def send_error(campaign)
+    CampaignMailer.error(campaign).deliver_now
   end
 
 end
